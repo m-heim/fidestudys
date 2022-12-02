56f468cba8b35801deea2ad3d900a4cfd84293cf
m-heim <MaximilianHeim@protonmail.com>
2022-12-02 17:58:15 +0100

    changes

--- analysis.py
+++ analysis.py
@@ -1,7 +1,21 @@
 import csv
+import numpy
+import os
+
+
 def main():
-    with open('player_list.csv', newline='\n') as csvfile:
-        data = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    print(data)
+    if not os.path.isfile("data.npy"):
+        with open('players_list.csv', newline='\n') as csvfile:
+            data = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            arr = numpy.array([row for row in data])
+            numpy.save("data.npy", arr, allow_pickle=True)
+    data = numpy.load("data.npy", allow_pickle=True)
+    #for row in data:
+    #    print(row)
+    print("\n".join([str(list(item)) for item in enumerate(data[0])]))
+    names = data[1:]
+    print(names)
+
+
 if __name__ == '__main__':
     main()
