2df2979e58964d4c97cf91764dc21a8b15940f9d
m-heim <MaximilianHeim@protonmail.com>
2022-12-02 17:33:52 +0100

    changes

--- analysis.py
+++ analysis.py
@@ -1,4 +1,7 @@
+import csv
 def main():
-    pass
+    with open('player_list.csv', newline='\n') as csvfile:
+        data = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    print(data)
 if __name__ == '__main__':
     main()
--- download.sh
+++ download.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-wget http://ratings.fide.com/download/players_list.zip
+if [ ! -f "players_list.zip" -a "players_list.csv" ]
+then
+    wget http://ratings.fide.com/download/players_list.zip
+    unzip players_list.zip
+    mv players_list_foa.txt players_list.csv
+fi
